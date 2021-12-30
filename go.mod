c974b60572d5d0068c1d3c2753137c6a92382c1f
Ridho Muhammad <50231796+Lacutee@users.noreply.github.com>
2021-12-30 12:04:55 +0700

    final

--- go.mod
+++ go.mod
@@ -4,8 +4,16 @@ go 1.17
 
 require (
 	github.com/cloudinary/cloudinary-go v1.5.0 // indirect
+	github.com/cosmtrek/air v1.27.8 // indirect
 	github.com/creasty/defaults v1.5.1 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"golang-cloudinary/cdn"
 	"log"
 	"net/http"
 
+	"github.com/cloudinary/cloudinary-go/api/uploader"
 	"github.com/gorilla/mux"
 )
 
@@ -14,10 +17,45 @@ func ServiceRouter(router *mux.Router) {
 		rw.Header().Set("Content-Type", "application/json")
 		rw.WriteHeader(http.StatusOK)
 		json.NewEncoder(rw).Encode("Welcome To Server")
-	})
-	router.HandleFunc("/upload", func(rw http.ResponseWriter, r *http.Request) {
+	}).Methods("GET")
 
-	})
+	router.HandleFunc("/upload", func(rw http.ResponseWriter, r *http.Request) {
+		file, _, err := r.FormFile("file")
+
+		// fileName := handler.Filename
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(rw).Encode(err)
+			fmt.Println(err)
+			return
+		}
+
+		defer file.Close()
+
+		cld, err := cdn.CdnSetting()
+		var ctx = context.Background()
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(rw).Encode(err)
+			fmt.Println("error open file", err)
+			return
+		}
+
+		uploadResult, err := cld.Upload.Upload(ctx, file, uploader.UploadParams{})
+
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(rw).Encode(err)
+			fmt.Println("error read file", err)
+			return
+		}
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		fmt.Println(uploadResult)
+		json.NewEncoder(rw).Encode(uploadResult.SecureURL)
+	}).Methods("POST")
 }
 
 func StartRouter() {
